d121bc30c5a8b6d5b6f2a4bb475ad26990b7e8ff
landoyjx <landoyjx@163.com>
2019-09-29 04:04:26 +0800

    fix int for code owner

--- src/call/app/tx/impl/SetAccount.cpp
+++ src/call/app/tx/impl/SetAccount.cpp
@@ -592,13 +592,14 @@ SetAccount::doApply ()
     if (ctx_.tx.isFieldPresent (sfCode))
     {
         auto uCode = ctx_.tx[sfCode];
-        size_t diff_size = uCode.size();
+        std::int32_t diff_size = uCode.size();
         if (sle->isFieldPresent(sfCode))
         {
             auto uOldCode = sle->getFieldVL(sfCode);
             diff_size = diff_size - uOldCode.size();
         }
-        auto codeCount = diff_size / view().fees().increment;
+        std::int32_t increment = view().fees().increment;
+        std::int32_t codeCount = diff_size / increment;
         sle->setFieldU32(sfOwnerCount, sle->getFieldU32(sfOwnerCount) + codeCount);
         sle->setFieldVL(sfCode, uCode);
     }
